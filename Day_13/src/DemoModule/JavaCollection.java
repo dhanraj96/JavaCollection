c3d012cd26bd6878c0b6474def8c127b9dd48ad1
Admin <Admin@DESKTOP-7E78KSE>
2022-02-23 18:58:05 +0530

    Java Collection

---
+++ Day_13/src/DemoModule/JavaCollection.java
@@ -0,0 +1,23 @@
+package DemoModule;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+
+
+public class JavaCollection {
+
+	public static void main(String[] args) {
+ LinkedList<Integer> oj = new LinkedList<>();
+
+		oj.add(10);
+		oj.add(20);
+		oj.add(30);
+		oj.add(40);
+		System.out.println("oj");
+		oj.addFirst(100);
+		System.out.println(oj);
+		
+	}
+
+}
---
+++ Day_13/src/DemoModule/String1.java
@@ -0,0 +1,20 @@
+package DemoModule;
+
+import java.util.ArrayList;
+
+public class String1 {
+
+	public static void main(String[] args) {
+	     ArrayList<String>Name = new ArrayList<String>();
+	     Name.add("Dhanraj");
+	     Name.add("Shivraj");
+	     Name.add("Sanket");
+	     System.out.println(Name);
+	     Name.add("Shubham");
+	     System.out.println(Name);
+	     
+	     
+
+	}
+
+}
